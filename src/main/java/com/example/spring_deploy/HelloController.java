7776f72a3d44ca19f3a10e668e906cb4bba991fb
Joan <joan-m-c@hotmail.com>
2024-06-01 19:02:33 -0400

    Spring heroku app config

---
+++ src/main/java/com/example/spring_deploy/HelloController.java
@@ -0,0 +1,47 @@
+package com.example.spring_deploy;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @Value("${app.message}")
+    String message;
+
+//    @Value("${app.varexample}")
+//    String Usuario;
+
+    @GetMapping("/hola")
+    public String holaMundo(){
+        System.out.println(message);
+        //System.out.println("Usuario: " + Usuario);
+
+        return "Hola Mundo que tal mundo";
+    }
+
+    @GetMapping("/bootstrap")
+    public String bootstrap(){
+        return """
+                <!doctype html>
+                <html lang="en">
+                  <head>
+                    <meta charset="utf-8">
+                    <meta name="viewport" content="width=device-width, initial-scale=1">
+                    <title>Bootstrap demo</title>
+                    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+                  </head>
+                  <body>
+                    <h1>Hola Mundo desde Spring Boot </h1>
+                    <a class="btn btn-primary" href="https://github.com/"> Github </a>
+                    
+                    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+                  </body>
+                </html>
+                
+                """;
+    }
+
+
+}
